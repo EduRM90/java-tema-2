dd9c41a26cde3544b44c8e4983a796b7045f10c1
Edu <luiseduardorm@gmail.com>
2022-12-16 16:58:52 +0100

    upload

---
+++ ejercicio2/src/ejercicio2/Ejercicio2.java
@@ -0,0 +1,16 @@
+package ejercicio2;
+import bpc.daw.mario.*;
+public class Ejercicio2 {
+    public static void main(String[] args) 
+    {
+        Mario mario1=new Mario(920, 540);
+        Luigi luigi1=new Luigi (960, 540);
+        Seta seta1=new Seta (880, 540);
+        Cañon cañon1=new Cañon (1000, 540);
+        Planta planta1=new Planta (1040, 540);
+        
+        mario1.saltar();
+        
+    }
+    
+}
